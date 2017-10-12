dfc4d0cb7af0527374095888ceae92bca5c3f1e2
tiwe <tiwe@microsoft.com>
2017-10-12 07:05:21 +0000

    bug fixed

--- main.py
+++ main.py
@@ -54,10 +54,10 @@ def main():
     # convert to numeric input outputs that fits into TF models
     train_feed = DataLoader("Train", train_dataset, config)
     valid_feed = DataLoader("Valid",
-                            random.sample(valid_dataset, np.min(valid_config.valid_size, len(valid_dataset))),
+                            random.sample(valid_dataset, min(valid_config.valid_size, len(valid_dataset))),
                             valid_config)
     test_feed = DataLoader("Test",
-                           random.sample(test_dataset, np.min(test_config.test_size, len(test_dataset))),
+                           random.sample(test_dataset, min(test_config.test_size, len(test_dataset))),
                            test_config)
 
     if FLAGS.forward_only or FLAGS.resume:
